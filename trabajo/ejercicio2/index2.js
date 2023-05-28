3ae2945d030cf55f4aa5c581e6e162d7c8cfbc83
FranksJavierVIlcaQuispe <franksjaviervilcaquispe@kali.Amirgould>
2023-05-28 01:15:29 -0500

    Terminado todos los ejercicios

--- trabajo/ejercicio2/index2.js
+++ trabajo/ejercicio2/index2.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-app.use(express.static('pub'));
+
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
 
